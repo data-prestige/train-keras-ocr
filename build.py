8f05355411c4599aa506d851ef990c71947c2318
Andrea Dodero <dodero.andrea@gmail.com>
2021-04-12 20:27:27 +0200

    a

--- build.py
+++ build.py
@@ -1,40 +1,78 @@
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Reshape, Dense, Dropout, Bidirectional, LSTM, Layer
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import RandomRotation, RandomContrast
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Reshape, Dense, Dropout, Bidirectional, LSTM, Layer, BatchNormalization, Lambda, add, concatenate
 from CTCLayer import CTCLayer
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import Model, Sequential
 
 
-def build_model(n_output, img_width, img_height):
-
-    # Input Stage
-    input_img = Input(shape=(img_width, img_height, 1), name="image", dtype="float32")                                  # Inputs to the model
-    labels = Input(name="label", shape=(None,), dtype="float32")
-    
-    # Convolutionary Feature Extractor 
-    x = Conv2D(32, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv1")(input_img)  # First conv block
-    x = MaxPooling2D((2, 2), name="pool1")(x)
-    x = Conv2D(64, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv2")(x)
-    x = MaxPooling2D((2, 2), name="pool2")(x)                                                                           # Second conv block
+# This is taken from: https://github.com/kurapan/CRNN/blob/master/models.py
+def ctc_lambda_func(args):
+    iy_pred, ilabels, iinput_length, ilabel_length = args
+    # the 2 is critical here since the first couple outputs of the RNN
+    # tend to be garbage:
+    iy_pred = iy_pred[:, 2:, :]  # no such influence
+    return tf.reduce_mean(tf.nn.ctc_loss(ilabels, iy_pred, ilabel_length, (iinput_length - 2), logits_time_major=False, blank_index=-1))
     
 
-    new_shape = ((img_width // 4), (img_height // 4) * 64)
-    x = Reshape(target_shape=new_shape, name="reshape")(x)
-    x = Dense(64, activation="relu", name="dense1")(x)
-    x = Dropout(0.2)(x)
-    
-    # Encoder Layer with BiLSTM 
-    x = Bidirectional(LSTM(128, return_sequences=True, dropout=0.25))(x)
-    x = Bidirectional(LSTM(64, return_sequences=True, dropout=0.25))(x)
-    
-    # Output layer
-    x = Dense(n_output, activation="softmax", name="dense2")(x)
-    output = CTCLayer(name="ctc_loss")(labels, x)                           # Add CTC layer for calculating CTC loss at each step
-    
-    # Define the model
-    deep_model = Model(inputs=[input_img, labels], outputs=output, name="end2end_ctc_loss_model")
-    
-    opt = Adam()                            # Optimizer
+def buildModel(img_width, img_height, n_output, opt):
+    augment = Sequential([
+        RandomContrast(0.1),
+        RandomRotation(0.1)
+    ])
+    # Model for prediction
+    input_img = Input((img_width, img_height, 1))
+    input_img_augmented = augment(input_img)
 
-    deep_model.compile(optimizer=opt)            # Compile the deep_model and return
+    # The model is adapted from the CRNN here: https://github.com/kurapan/CRNN/blob/master/models.py
+    c_1 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv_1')(input_img_augmented)
+    c_2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv_2')(c_1)
+    c_3 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv_3')(c_2)
+    bn_3 = BatchNormalization(name='bn_3')(c_3)
+    p_3 = MaxPooling2D(pool_size=(2, 2), name='maxpool_3')(bn_3)
 
-    return deep_model
+    c_4 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv_4')(p_3)
+    c_5 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_5')(c_4)
+    bn_5 = BatchNormalization(name='bn_5')(c_5)
+    p_5 = MaxPooling2D(pool_size=(2, 2), name='maxpool_5')(bn_5)
+
+    c_6 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_6')(p_5)
+    c_7 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_7')(c_6)
+    bn_7 = BatchNormalization(name='bn_7')(c_7)
+
+    bn_7_shape = bn_7.get_shape()
+    reshape = Reshape(target_shape=(int(bn_7_shape[1]), int(bn_7_shape[2] * bn_7_shape[3])), name='reshape')(bn_7)
+
+    fc_9 = Dense(128, activation='relu', name='fc_9')(reshape)
+
+    lstm_10 = LSTM(128, kernel_initializer="he_normal", return_sequences=True, name='lstm_10')(fc_9)
+    lstm_10_back = LSTM(128, kernel_initializer="he_normal", go_backwards=True, return_sequences=True, name='lstm_10_back')(fc_9)
+    lstm_10_add = add([lstm_10, lstm_10_back])
+
+    lstm_11 = LSTM(128, kernel_initializer="he_normal", return_sequences=True, name='lstm_11')(lstm_10_add)
+    lstm_11_back = LSTM(128, kernel_initializer="he_normal", go_backwards=True, return_sequences=True, name='lstm_11_back')(lstm_10_add)
+    lstm_11_concat = concatenate([lstm_11, lstm_11_back])
+    do_11 = Dropout(0.25, name='dropout')(lstm_11_concat)
+
+    pred = Dense(n_output + 1, kernel_initializer='he_normal', activation=None, name='fc_12')(do_11)
+    # This is the model to be used for prediction. It only takes the images as input, and outputs
+    # the corresponding predictions (logits).
+    prediction_model = Model(inputs=input_img, outputs=pred)
+
+    # The model for training will take three more inputs, i.e., the labels, xb_len, and yb_len.
+    labels = Input(name='labels', shape=[None], dtype='int64')
+    input_length = Input(name='input_length', shape=[], dtype='int64')
+    label_length = Input(name='label_length', shape=[], dtype='int64')
+
+    # The layer simply outputs the loss
+    ctc_loss = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([pred, labels, input_length, label_length])
+    # This is extremely strange to read, but it is required because of the way .fit() works.
+    # Building a custom training loop (or overriding train_step) would be a much better solution).
+    # Basically, this model takes all the inputs and outputs the CTC loss.
+    training_model = Model(inputs=[input_img, labels, input_length, label_length], outputs=[ctc_loss], name="end2end_ctc_loss_model")
+
+    # Again, this can be improved... The first parameter here is a sequence of zeros, that we returned in our generator. The second parameter (the output of training_model) is the actual loss.
+    training_model.compile(loss={'ctc': lambda _, ctc_loss: ctc_loss}, optimizer=opt)
+
+    return training_model, prediction_model
--- train_.py
+++ train_.py
@@ -8,6 +8,7 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Reshape, Dense,
 from tensorflow.keras import Model, Sequential
 import tensorflow.keras.backend as K
 from tensorflow.keras.optimizers import Adam, SGD
+from build import buildModel, ctc_lambda_func
 
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
 for device in gpu_devices:
@@ -48,17 +49,15 @@ def process_path(image_path, image_name):
     img = tf.image.decode_jpeg(img, channels=1)
     img = tf.image.resize(img, [img_height, img_width])
     img = tf.image.flip_left_right(img)
-    img = tf.cast(img[:, :, 0], tf.float32) / 255.0 # Normalization
+    img = tf.cast(img[:,:, 0], tf.float32) / 255.0  # Normalization
     img = tf.transpose(img, [1, 0])
     img = img[:, :, tf.newaxis]
-
     # Get the label and its length
     label = tf.strings.split(image_name, '_')[0]
     label = tf.strings.upper(label)
     label_len = tf.strings.length(label)
 
     return (img, tf.strings.bytes_split(label), img_width // reduction_factor, label_len), tf.zeros(1)
-
 
 # Apply the preprocessing to each image
 dataset = dataset.map(process_path, num_parallel_calls=tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE)
@@ -76,96 +75,24 @@ def convert_string(xb, yb):
 
 dataset = dataset.map(convert_string, num_parallel_calls=tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE)
 val_dataset = val_dataset.map(convert_string, num_parallel_calls=tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE)
-
-augment = Sequential([
-    RandomContrast(0.1),
-    RandomRotation(0.1)
-])
-
+# opt = Adam()
+opt = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
 n_output = len(lookup.get_vocabulary())
-# Model for prediction
-input_img = Input((img_width, img_height, 1))
-input_img_augmented = augment(input_img)
-
-# The model is adapted from the CRNN here: https://github.com/kurapan/CRNN/blob/master/models.py
-c_1 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv_1')(input_img_augmented)
-c_2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv_2')(c_1)
-c_3 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv_3')(c_2)
-bn_3 = BatchNormalization(name='bn_3')(c_3)
-p_3 = MaxPooling2D(pool_size=(2, 2), name='maxpool_3')(bn_3)
-
-c_4 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv_4')(p_3)
-c_5 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_5')(c_4)
-bn_5 = BatchNormalization(name='bn_5')(c_5)
-p_5 = MaxPooling2D(pool_size=(2, 2), name='maxpool_5')(bn_5)
-
-c_6 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_6')(p_5)
-c_7 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv_7')(c_6)
-bn_7 = BatchNormalization(name='bn_7')(c_7)
-
-bn_7_shape = bn_7.get_shape()
-reshape = Reshape(target_shape=(int(bn_7_shape[1]), int(bn_7_shape[2] * bn_7_shape[3])), name='reshape')(bn_7)
-
-fc_9 = Dense(128, activation='relu', name='fc_9')(reshape)
-
-lstm_10 = LSTM(128, kernel_initializer="he_normal", return_sequences=True, name='lstm_10')(fc_9)
-lstm_10_back = LSTM(128, kernel_initializer="he_normal", go_backwards=True, return_sequences=True, name='lstm_10_back')(fc_9)
-lstm_10_add = add([lstm_10, lstm_10_back])
-
-lstm_11 = LSTM(128, kernel_initializer="he_normal", return_sequences=True, name='lstm_11')(lstm_10_add)
-lstm_11_back = LSTM(128, kernel_initializer="he_normal", go_backwards=True, return_sequences=True, name='lstm_11_back')(lstm_10_add)
-lstm_11_concat = concatenate([lstm_11, lstm_11_back])
-do_11 = Dropout(0.25, name='dropout')(lstm_11_concat)
-
-pred = Dense(n_output + 1, kernel_initializer='he_normal', activation=None, name='fc_12')(do_11)
-# This is the model to be used for prediction. It only takes the images as input, and outputs
-# the corresponding predictions (logits).
-prediction_model = Model(inputs=input_img, outputs=pred)
-
-# The model for training will take three more inputs, i.e., the labels, xb_len, and yb_len.
-labels = Input(name='labels', shape=[None], dtype='int64')
-input_length = Input(name='input_length', shape=[], dtype='int64')
-label_length = Input(name='label_length', shape=[], dtype='int64')
-
-# This is taken from: https://github.com/kurapan/CRNN/blob/master/models.py
-def ctc_lambda_func(args):
-    iy_pred, ilabels, iinput_length, ilabel_length = args
-    # the 2 is critical here since the first couple outputs of the RNN
-    # tend to be garbage:
-    iy_pred = iy_pred[:, 2:, :]  # no such influence
-    # return K.ctc_batch_cost(ilabels, tf.nn.softmax(iy_pred, 2), (iinput_length - 2), ilabel_length)
-    return tf.reduce_mean(tf.nn.ctc_loss(ilabels, iy_pred, ilabel_length, (iinput_length - 2), logits_time_major=False, blank_index=-1))
-
-
-# The layer simply outputs the loss
-ctc_loss = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([pred, labels, input_length, label_length])
-
-# This is extremely strange to read, but it is required because of the way .fit() works.
-# Building a custom training loop (or overriding train_step) would be a much better solution).
-# Basically, this model takes all the inputs and outputs the CTC loss.
-training_model = Model(inputs=[input_img, labels, input_length, label_length], outputs=[ctc_loss], name="end2end_ctc_loss_model")
-opt = Adam()
-# opt = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
-
-# Again, this can be improved... The first parameter here is a sequence of zeros, that we returned in our generator. The second parameter (the output of training_model) is the actual loss.
-training_model.compile(loss={'ctc': lambda _, ctc_loss: ctc_loss}, optimizer=opt)
+training_model, _ = buildModel(img_width, img_height, n_output, opt)
 
 # For the training dataset, we apply shuffling and batching. Any data augmentation should go here.
 train_dataset = dataset.shuffle(1000).padded_batch(32)
 val_dataset = val_dataset.padded_batch(32)
-
 default_callbacks = []
-
 # Add save best model
 checkPoint = ModelCheckpoint("cnn_bilstm_ctc_loss_end2end_model.h5", save_weights_only=False, monitor="val_loss", verbose=1, save_best_only=True, mode='min')
 default_callbacks = default_callbacks + [checkPoint]
-
 # Add early stopping
 lr_reducer = ReduceLROnPlateau(factor=0.1, patience=3, verbose=1, min_lr=0.00001)
 earlyStopping = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=10, verbose=0, restore_best_weights=True, mode='min') 
 default_callbacks = default_callbacks + [earlyStopping]
+history = training_model.fit(train_dataset, validation_data=val_dataset, epochs=1, callbacks=default_callbacks)
 
-history = training_model.fit(train_dataset, validation_data=val_dataset, epochs=5, callbacks=default_callbacks)
 
 for (xb, yb, xb_len, yb_len), _ in val_dataset:
     print(yb)
@@ -176,4 +103,5 @@ y_pred = prediction_model.predict(xb)
 y_pred = tf.transpose(y_pred, [1, 0, 2])[2:,:,:]  # Transpose the first dimension and remove the first two time-steps
 (y_decoded, _) = tf.nn.ctc_greedy_decoder(y_pred, sequence_length=tf.ones(y_pred.shape[1], dtype=tf.int32)*y_pred.shape[0])
 y_decoded_text = inv_lookup(y_decoded[0])
-tf.sparse.to_dense(y_decoded_text)
+results = tf.sparse.to_dense(y_decoded_text)
+print(results)
