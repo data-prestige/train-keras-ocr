281b1af8b6272f11494de6f3441253dc488306ad
Andrea Dodero <dodero.andrea@gmail.com>
2021-05-18 13:41:28 +0200

    add requirements

--- train_keras_ocr.py
+++ train_keras_ocr.py
@@ -20,8 +20,6 @@ from EditDistanceCallback import EditDistanceCallback
 """## Data loading (tf.data)"""
 
 # Find all the images inside the folder (only the name)
-
-
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
 for device in gpu_devices:
     tf.config.experimental.set_memory_growth(device, True)
